0671be889897c0ae4ada9ac49e545d50f1f470aa
bdaniel7 <bdaniel7@gmail.com>
2015-10-17 10:56:14 +0300

    - requirejs

---
+++ src/js/requirejs-config.js
@@ -0,0 +1,18 @@
+/**
+ * Created by daniel on 30.09.2015.
+ */
+
+var require = {
+    baseUrl: ".", // js is the base path
+    paths: {
+        "jquery":       "../bower_components/jquery/dist/jquery",
+        "bootstrap":    "../bower_components/bootstrap/dist/bootstrap.min",
+        "underscore":   "../bower_components/underscore/underscore",
+        "backbone":     "../bower_components/backbone/backbone",
+        "text": "js/vendor/require.text",
+        "domReady": "js/vendor/require.domReady",
+    },
+    shim: {
+        "bootstrap": { deps: ["jquery"] }
+    }
+};
